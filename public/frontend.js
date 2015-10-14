d769a4677aa24c99cdd04a90514e023f5e6e8856
Justen <justenbarget@gmail.com>
2015-10-14 15:10:06 +0100

    adding login page to link in index.html

--- public/frontend.js
+++ public/frontend.js
@@ -1,10 +1,13 @@
-function getValue(input){
-  document.getElementById(input).value;
-}
-
-getValue('username');
-getValue('password');
-
-// function createJWT(username, password){
+// var jwt = require('jsonwebtoken');
+//
+// function createJWT(input){
+//   var username = document.getElementById('username').value;
+//   var password = document.getElementById('password').value;
+//   var obj = {
+//     'username': username;
+//     'password': password
+//   }
+//
+//   var token = jwt.sign(obj, 'shhhh');
 //
 // }
