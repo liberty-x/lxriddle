6c79f1ce2730a573eaa8b4114c756d64b41c8a0d
Rachel Black <rachel.black1984@googlemail.com>
2015-10-14 16:40:42 +0100

    tests for handler and dabase

--- test/backendTest.js
+++ test/backendTest.js
@@ -4,15 +4,45 @@ var shot = require('shot');
 
 test("check server is running", function(t){
 
-var request = {
-  method:'GET',
-  url: '/'
-};
+  var request = {
+    method:'GET',
+    url: '/'
+  };
 
   shot.inject(server.handler, request, function(res){
     var result = res.statusCode;
     var expected = 200;
     t.equal(expected, result, "server is up and running");
+    t.end();
+  });
+});
+
+
+test("check handler can process files", function(t){
+
+  var request = {
+    method:'GET',
+    url: '/public/style.css'
+  };
+
+  shot.inject(server.handler, request, function(res){
+    var result = res.statusCode;
+    var expected = 200;
+    t.equal(expected, result, "handler ready to process files");
+    t.end();
+  });
+});
+
+test("has the riddle been posted to the database", function(t){
+  var request = {
+    method: 'POST',
+    url: '/' + "What goes down but never goes up" + "/" + "rain"
+  }
+
+  shot.inject(server.handler, request, function(res){
+    var result = res.payload;
+    var expected = 'OK'
+    t.equal(expected, result, "Riddle is posted to DB");
     t.end();
     server.client.quit();
   });
